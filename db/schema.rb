8f16e9f336ffe94a53e8cc4edd19574848af8a55
David Wen <dstwen@gmail.com>
2015-01-17 14:56:47 -0800

    added campaigns table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117205515) do
+ActiveRecord::Schema.define(version: 20150117224342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "campaigns", force: true do |t|
+    t.string "title"
+    t.text   "description"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name",             default: "", null: false
