8f25f18721c2dcfbbc34630509022069496cf0cc
David Wen <dstwen@gmail.com>
2015-01-17 16:49:13 -0800

    add about page and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   root "pages#landing"
+
+  get "about" => "pages#about", as: :about
 end
