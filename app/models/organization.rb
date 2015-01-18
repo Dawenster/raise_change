433f76ad075b74815026b293776e9103635a8c60
David Wen <dstwen@gmail.com>
2015-01-18 11:03:11 -0800

    validate org has valid website url

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,9 +1,22 @@
+require 'uri'
+
 class Organization < ActiveRecord::Base
   validates :name, presence: true
+  validate :valid_url
 
   has_many :contacts
   has_many :campaigns
   has_many :users, :through => :campaigns
   accepts_nested_attributes_for :campaigns
   accepts_nested_attributes_for :contacts
+
+  def valid_url
+    return true if website.blank?
+    uri = URI.parse(website)
+    unless uri.kind_of?(URI::HTTP)
+      errors.add(:website, "must be blank or a valid url")
+    end
+  rescue URI::InvalidURIError
+    errors.add(:website, "must be blank or a valid url")
+  end
 end
