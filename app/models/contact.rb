c08b3934422e5069e4ed7ee47866cde382af5ae2
David Wen <dstwen@gmail.com>
2015-01-18 11:05:12 -0800

    add email validation on contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ActiveRecord::Base
   validates :name, :email, presence: true
+  validates :email, :format => { :with => /^\s*(([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})[\s\/,;]*)+$/i, :message => "is not a valid email", :multiline => true }
 
   belongs_to :organization
 end
