c0815a907296f928941b332a14df7d17e2642287
David Wen <dstwen@gmail.com>
2015-01-18 17:11:07 -0800

    remove debugger

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
   $('body').on('click', '.navbar-avatar', function (event, tab) {
     $(".user-account-dropdown").dropdown("toggle")
-    debugger
-    $(this)
     return false;
   });
 });
