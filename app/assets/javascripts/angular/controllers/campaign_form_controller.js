ecc40e204203ad93c2a82daf79c46f67dd195261
David Wen <dstwen@gmail.com>
2015-01-24 10:32:12 -0800

    add toggle for validation section on campaign form

--- app/assets/javascripts/angular/controllers/campaign_form_controller.js
+++ app/assets/javascripts/angular/controllers/campaign_form_controller.js
@@ -2,6 +2,7 @@ var app = angular.module('raisechange');
 
 app.controller('CampaignFormCtrl', ['$scope', function($scope) {
   initialWebsiteLinkText();
+  initialValidationSectionText();
 
   $scope.websiteLinkToggle = function() {
     $scope.websiteField = !$scope.websiteField;
@@ -13,14 +14,34 @@ app.controller('CampaignFormCtrl', ['$scope', function($scope) {
     }
   }
 
+  $scope.validationSectionToggle = function() {
+    $scope.validationSection = !$scope.validationSection;
+    if ($scope.validationSection) {
+      $scope.validationSectionText = "Remove validator";
+    } else {
+      $("#organization_contacts_attributes_0_name").val("");
+      $("#organization_contacts_attributes_0_email").val("");
+      initialValidationSectionText()
+    }
+  }
+
   function initialWebsiteLinkText() {
-    var pageType = $(".open-website-form-link").attr("data-page-type")
+    var pageType = $(".campaign-form").attr("data-page-type")
     if (pageType == "hasSite") {
       $scope.websiteField = true;
-      $scope.websiteFieldText = "Edit website (optional)";
     } else {
       $scope.websiteField = false;
       $scope.websiteFieldText = "Add website (optional)";
+    }
+  }
+
+  function initialValidationSectionText() {
+    var pageType = $(".organization-validation-form-section").attr("data-page-type")
+    if (pageType == "hasContact") {
+      $scope.validationSection = true;
+    } else {
+      $scope.validationSection = false;
+      $scope.validationSectionText = "Add validator (optional)";
     }
   }
 }]);
