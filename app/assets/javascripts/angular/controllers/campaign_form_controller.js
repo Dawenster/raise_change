0f112f1205e49736384da26994815b78c3c4d6ae
David Wen <dstwen@gmail.com>
2015-01-24 10:09:26 -0800

    add toggle for website field

--- app/assets/javascripts/angular/controllers/campaign_form_controller.js
+++ app/assets/javascripts/angular/controllers/campaign_form_controller.js
@@ -1,5 +1,26 @@
 var app = angular.module('raisechange');
 
 app.controller('CampaignFormCtrl', ['$scope', function($scope) {
-  
+  initialWebsiteLinkText();
+
+  $scope.websiteLinkToggle = function() {
+    $scope.websiteField = !$scope.websiteField;
+    if ($scope.websiteField) {
+      $scope.websiteFieldText = "Remove website";
+    } else {
+      $("#organization_website").val("");
+      initialWebsiteLinkText()
+    }
+  }
+
+  function initialWebsiteLinkText() {
+    var pageType = $(".open-website-form-link").attr("data-page-type")
+    if (pageType == "hasSite") {
+      $scope.websiteField = true;
+      $scope.websiteFieldText = "Edit website (optional)";
+    } else {
+      $scope.websiteField = false;
+      $scope.websiteFieldText = "Add website (optional)";
+    }
+  }
 }]);
