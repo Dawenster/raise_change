c4e3e2706c1bf571643fbf7d3ec87330a0a15a86
David Wen <dstwen@gmail.com>
2015-01-24 23:17:15 -0800

    toggle delete account on edit user account

--- app/assets/javascripts/angular/controllers/update_user_controller.js
+++ app/assets/javascripts/angular/controllers/update_user_controller.js
@@ -2,4 +2,5 @@ var app = angular.module('raisechange.controllers', []);
 
 app.controller('UpdateUserCtrl', ['$scope', function($scope) {
   $scope.showPassword = false;
+  $scope.showCancelAccount = false;
 }]);
