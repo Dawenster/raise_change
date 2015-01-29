7e30ebc17adaf029b41b82a07994a95e7690c7f8
David Wen <dstwen@gmail.com>
2015-01-29 07:47:25 -0800

    fix for https images by paperclip

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -7,16 +7,15 @@ class Campaign < ActiveRecord::Base
 
   s3_credentials_hash = {
     :access_key_id => ENV['AWS_ACCESS_KEY'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :s3_permissions => :public_read,
-    :s3_protocol => :https
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 
   has_attached_file :image, 
                     :styles => { :thumb => "300x225#", :display => "720x540#" },
                     :s3_credentials => s3_credentials_hash,
                     :bucket => ENV['AWS_BUCKET'],
-                    :default_url => "https://s3-us-west-2.amazonaws.com/timeauction/missing-auction-thumb.png"
+                    :default_url => "https://s3-us-west-2.amazonaws.com/timeauction/missing-auction-thumb.png",
+                    :s3_protocol => :https
 
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,16 +11,15 @@ class User < ActiveRecord::Base
 
   s3_credentials_hash = {
     :access_key_id => ENV['AWS_ACCESS_KEY'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :s3_permissions => :public_read,
-    :s3_protocol => :https
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 
   has_attached_file :avatar, 
                     :styles => { :small => "40x40#", :large => "200x200#" },
                     :s3_credentials => s3_credentials_hash,
                     :bucket => ENV['AWS_BUCKET'],
-                    :default_url => "https://s3-us-west-2.amazonaws.com/raisechange/general/no-profile-image.png"
+                    :default_url => "https://s3-us-west-2.amazonaws.com/raisechange/general/no-profile-image.png",
+                    :s3_protocol => :https
 
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
