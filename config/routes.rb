3a0031db79ed772d4f358c62e0346a7982038d2f
David Wen <dstwen@gmail.com>
2015-02-01 10:21:25 -0800

    add journal entry routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
   get ":campaign_id/donate" => "donations#new", as: :new_donation
   resources :donations, :only => [:create, :destroy]
 
+  get "journal-entries" => "journal_entries#index", as: :journal_entries
+  get "create-journal-entry" => "journal_entries#new", as: :new_journal_entry
+  get "edit-journal-entry/:id" => "journal_entries#edit", as: :edit_journal_entry
+  resources :journal_entries, :only => [:create, :update, :destroy]
+
   post "create-or-update-credit-card" => "users#create_or_update_credit_card", as: :create_or_update_credit_card
   delete "delete-credit-card" => "users#delete_credit_card", as: :delete_credit_card
   resources :users, :only => [:show]
