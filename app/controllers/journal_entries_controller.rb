1e18e64badd787afb65ce8fdfe4617aec851be9b
David Wen <dstwen@gmail.com>
2015-02-01 10:21:38 -0800

    journal entries controller initial setup

---
+++ app/controllers/journal_entries_controller.rb
@@ -0,0 +1,59 @@
+class JournalEntriesController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @journal_entries = JournalEntry.all
+  end
+
+  def new
+    @journal_entry = JournalEntry.new
+  end
+
+  def create
+    @journal_entry = JournalEntry.new(journal_entry_params)
+    if @journal_entry.save
+      campaign = @journal_entry.campaign
+      flash[:notice] = "Journal entry created successfully for \"#{campaign.title}\"."
+      redirect_to journal_entries_path
+    else
+      flash.now[:alert] = @journal_entry.errors.full_messages.join(". ") + "."
+      render "new"
+    end
+  end
+
+  def edit
+    @journal_entry = JournalEntry.find(params[:id])
+  end
+
+  def update
+    @journal_entry = JournalEntry.find(params[:id])
+    @journal_entry.assign_attributes(journal_entry_params)
+    if @journal_entry.save
+      campaign = @journal_entry.campaign
+      flash[:notice] = "Journal entry updated successfully for \"#{campaign.title}\"."
+      redirect_to journal_entries_path
+    else
+      flash.now[:alert] = @journal_entry.errors.full_messages.join(". ") + "."
+      render "edit"
+    end
+  end
+
+  def destroy
+    journal_entry = JournalEntry.find(params[:id]).destroy
+    flash[:notice] = "Journal entry successfully deleted."
+    redirect_to journal_entries_path
+  end
+
+  private 
+
+  def journal_entry_params
+    params.require(:journal_entry).permit(
+      :dates,
+      :hours,
+      :description,
+      :user_id,
+      :campaign_id,
+      :_destroy
+    )
+  end
+end
