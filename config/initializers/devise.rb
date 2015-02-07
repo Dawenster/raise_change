377bd514b3830ac4920a38d8b0cbf0b70ca596cb
David Wen <dstwen@gmail.com>
2015-02-07 14:12:45 -0800

    prettify signup confirmation email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   config.mailer_sender = '"Raise Change Team" <team@raisechange.com>'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'RegistrationMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
