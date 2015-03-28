692f88aa0b3dee319d18704dc62a95c0cfce0c17
David Wen <dstwen@gmail.com>
2015-03-28 13:51:36 -0700

    new design: how it works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,6 @@
 class PagesController < ApplicationController
+  def how_it_works
+    @title = "How it <span class='highlight'>works</span>"
+    @image = "https://s3-us-west-2.amazonaws.com/raisechange/general/volunteer.jpg"
+  end
 end
