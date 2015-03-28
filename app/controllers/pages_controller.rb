d9a84a5cee9a490f374cd34fea20f0de77e2f1ef
David Wen <dstwen@gmail.com>
2015-03-28 14:27:18 -0700

    new design: about

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
     @title = "How it <span class='highlight'>works</span>"
     @image = "https://s3-us-west-2.amazonaws.com/raisechange/general/volunteer.jpg"
   end
+
+  def about
+    @title = "￼Raise Change is a tool to <span class='highlight'>record</span> your story as you help <span class='highlight'>influence</span> and <span class='highlight'>communicate</span> change."
+    @image = "https://s3-us-west-2.amazonaws.com/raisechange/general/volunteer2.jpg"
+  end
 end
