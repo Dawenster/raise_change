c0ee0f8182dbb8aa773136f6734ea071ac66a7cc
David Wen <dstwen@gmail.com>
2015-05-08 20:39:29 -0700

    change text from journal entry to log hours

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -5,7 +5,7 @@ class JournalEntriesController < ApplicationController
     @journal_entry = JournalEntry.new
     @journal_entry.media_files.build
     @campaigns = current_user.campaigns
-    @title = "Create <span class='highlight'>journal entry</span>"
+    @title = "Log <span class='highlight'>hours</span>"
     @image = "https://s3-us-west-2.amazonaws.com/raisechange/general/generic_soccer.jpg"
     @center = true
   end
@@ -15,7 +15,7 @@ class JournalEntriesController < ApplicationController
     @journal_entry.user_id = current_user.id
     if @journal_entry.save
       campaign = @journal_entry.campaign
-      flash[:notice] = "Journal entry created successfully for \"#{campaign.title}\"."
+      flash[:notice] = "Hours logged successfully for \"#{campaign.title}\"."
       redirect_to campaign_path(@journal_entry.campaign)
     else
       @campaigns = current_user.campaigns
@@ -27,7 +27,7 @@ class JournalEntriesController < ApplicationController
   def edit
     @journal_entry = JournalEntry.find(params[:id])
     @campaigns = current_user.campaigns
-    @title = "Edit <span class='highlight'>journal entry</span>"
+    @title = "Edit <span class='highlight'>hours log</span>"
     @image = "https://s3-us-west-2.amazonaws.com/raisechange/general/generic_soccer.jpg"
     @center = true
   end
@@ -37,7 +37,7 @@ class JournalEntriesController < ApplicationController
     @journal_entry.assign_attributes(journal_entry_params)
     if @journal_entry.save
       campaign = @journal_entry.campaign
-      flash[:notice] = "Journal entry updated successfully for \"#{campaign.title}\"."
+      flash[:notice] = "Hours log updated successfully for \"#{campaign.title}\"."
       redirect_to campaign_path(@journal_entry.campaign)
     else
       @campaigns = current_user.campaigns
@@ -50,7 +50,7 @@ class JournalEntriesController < ApplicationController
     journal_entry = JournalEntry.find(params[:id])
     path_to_go_to_after = campaign_path(journal_entry.campaign)
     journal_entry.destroy
-    flash[:notice] = "Journal entry successfully deleted."
+    flash[:notice] = "Hours log successfully deleted."
     redirect_to path_to_go_to_after
   end
 
