2f8a3cb9f5c9a77f8d9969a166a7559a686330a4
David Wen <dstwen@gmail.com>
2015-07-31 11:34:07 -0700

    Styled image select on campaigns

--- app/assets/javascripts/angular/controllers/campaign_form_controller.js
+++ app/assets/javascripts/angular/controllers/campaign_form_controller.js
@@ -53,4 +53,28 @@ app.controller('CampaignFormCtrl', ['$scope', function($scope) {
       $scope.validationSectionText = "Add validator (optional)";
     }
   }
+
+  // To so feedback after image file select
+
+  $(".general-form-holder").on('change', '.btn-file :file', function() {
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+  });
+
+  $(".general-form-holder").ready( function() {
+    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+      var input = $(this).parents('.input-group').find(':text'),
+          log = numFiles > 1 ? numFiles + ' files selected' : label;
+      
+      if (input.length) {
+        input.val(log);
+      } else {
+        if (log) {
+          $(".selected-image-file-name").text(log)
+        }
+      }
+    });
+  });
 }]);
