2bad690bee8b7106e3521ec59b74d30ee43ba2bd
David Wen <dstwen@gmail.com>
2015-07-31 12:50:09 -0700

    Add slug column to organizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150731011801) do
+ActiveRecord::Schema.define(version: 20150731194912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150731011801) do
     t.string   "website"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
 
   create_table "relationships", force: true do |t|
