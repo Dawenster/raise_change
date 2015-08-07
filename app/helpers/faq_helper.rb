297a8d63494254ae45e5b0115dae34e7a0e3bf37
Ben Torn <tornben@gmail.com>
2015-08-07 16:11:28 -0700

    Updated FAQ to include more questions

--- app/helpers/faq_helper.rb
+++ app/helpers/faq_helper.rb
@@ -8,13 +8,19 @@ module FaqHelper
           {
             :question => "How much do you charge?",
             :answer_paragraphs => [
-              "We collect a 5% fee on donations in order to keep the website running and feed ourselves. We also collect a 2.9% + $0.30 processing fees on donations in order to pay the stripe fees that cover credit card costs."
+              "We collect a 5% fee on donations in order to keep the website running and feed ourselves. We also collect a 2.9% + $0.30 processing fees on donations in order to pay the stripe fees that cover credit card costs. This amount will be deducted from your donation pledge automatically."
             ]
           },
           {
-            :question => "What about the sites that claim not to charge anything?",
+            :question => "What about the sites that claim not to charge a fee?",
             :answer_paragraphs => [
               "No site runs for free. Most crowdsourcing websites charge between 5-10% plus processing fees. However, some websites claim not to charge anything, which is a bit misleading. They still charge the same processing fees and run “on donations”, which are actually automatically added on to your donation and have to be opted out via multiple steps during the donation process."
+            ]
+          },
+          {
+            :question => "Are you a not-for-profit?",
+            :answer_paragraphs => [
+              "Raise Change is a social enterprise that aligns its business goals with doing good."
             ]
           }
         ]
@@ -24,9 +30,15 @@ module FaqHelper
         :id => "vol",
         :questions => [
           {
+            :question => "Will I get paid to volunteer?",
+            :answer_paragraphs => [
+              "No. All the money from the donors goes directly to the organization you volunteered at or a related nonprofit"
+            ]
+          },
+          {
             :question => "How does the nonprofit get the money I raised?",
             :answer_paragraphs => [
-              "At the end of the #{donation_period}, we will mail the campaign creator (you) a cheque for the amount that was collected from the donors. That way, you can present the cheque it to the nonprofit yourself :)"
+              "At the end of each month, we will mail the campaign creator (you) a cheque for the amount that was collected from the donors. That way, you can present the cheque to the nonprofit yourself :)"
             ]
           },
           {
@@ -45,7 +57,13 @@ module FaqHelper
           {
             :question => "When do you charge my credit card?",
             :answer_paragraphs => [
-              "Donations are charged #{donation_period}."
+              "Donations are charged at the end of each month."
+            ]
+          },
+          {
+            :question => "Can I set the maximum donation amount?",
+            :answer_paragraphs => [
+              "Yes. During the donation process, you can set a maximum amount you are willing to donate per month. Once the volunteer reaches that amount, you will no longer be charged during that month. When you no longer want to pledge your support, simply cancel your pledge and you won't be charged from that point."
             ]
           },
           {
@@ -55,11 +73,18 @@ module FaqHelper
             ]
           },
           {
+            :question => "Is my credit card information safe?",
+            :answer_paragraphs => [
+              "Yes, all credit card information is encrypted. We use Stripe to process all payments, which is one of the biggest credit card payment companies. Other companies that use Stripe include Twitter, Kickstarter and Salesforce.com."
+            ]
+          },
+          {
             :question => "Can I cancel my donation pledge?",
             :answer_paragraphs => [
-              "Yes. If you change your mind at any point, you can simply log into your account and cancel your pledge (but your mom would be disappointed in you)."
+              "Yes. If you change your mind at any point, you can simply log into your account and cancel your pledge (but your mom will be disappointed in you)."
             ]
           }
+
         ]
       }
     ]
