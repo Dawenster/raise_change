bf0764162979695848deb0d82e908660d9a89c40
David Wen <dstwen@gmail.com>
2015-08-10 09:34:30 -0700

    Update admin index view for campaigns

--- app/admin/campaign.rb
+++ app/admin/campaign.rb
@@ -9,4 +9,15 @@ ActiveAdmin.register Campaign do
     :estimated_hours,
     :frequency
   )
+
+  index :as => ActiveAdmin::Views::IndexAsTable do
+    column :title
+    column :description
+    column :user
+    column :organization
+    column :estimated_hours
+    column :frequency
+    column :created_at
+    column :updated_at
+  end
 end
