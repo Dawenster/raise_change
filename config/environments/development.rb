5b3347631ab1b4c8434019210d714aad5d554cd9
David Wen <dstwen@gmail.com>
2015-08-17 09:53:19 -0700

    Add commented out sample live email details

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,16 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  # Gmail Settings
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'raisechange.com',
+  #   user_name:            'team@raisechange.com',
+  #   password:             "blahblahblah",
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
 end
